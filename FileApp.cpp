9505efb48ad31d6cf092298ea899fd08e426dcdf
idzipris <idzipris@ebay.com>
2019-06-17 14:28:51 +0300

    Initialization of the project #2

--- FileApp.cpp
+++ FileApp.cpp
@@ -1,6 +1,10 @@
 //
 // Created by idzipris on 6/17/2019.
 //
-
+#include <iostream>
 #include "FileApp.h"
 
+int main(int argc, char** argv) {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
