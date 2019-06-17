96a06adc947991703fa3372e0e28a0836fe9b973
idzipris <idzipris@ebay.com>
2019-06-17 14:15:01 +0300

    Initialization of the project

---
+++ FileApp.cpp
@@ -0,0 +1,6 @@
+//
+// Created by idzipris on 6/17/2019.
+//
+
+#include "FileApp.h"
+
---
+++ FileApp.h
@@ -0,0 +1,8 @@
+//
+// Created by idzipris on 6/17/2019.
+//
+
+#ifndef FILEAPP_FILEAPP_H
+#define FILEAPP_FILEAPP_H
+
+#endif //FILEAPP_FILEAPP_H
